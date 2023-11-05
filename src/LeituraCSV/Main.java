cc8af7df365dd310f3357b96280d21f5b3e28679
Amanda Machado <amanda.ramos25@hotmail.com>
2023-11-05 14:02:26 -0300

    part3completa

--- src/LeituraCSV/Main.java
+++ src/LeituraCSV/Main.java
@@ -1,5 +1,6 @@
 package LeituraCSV;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,26 +9,100 @@ public class Main {
         
         Scanner sc = new Scanner(System.in);
         Ordenacao od = new Ordenacao();
+        LerArquivo lr = new LerArquivo();
 
-        System.out.println("-------------------\nDigite qual metodo de ordenação deseja fazer: \n1. Seleção pelo nome da música\n2. Inserção pelo nome da música \n3.Seleção pelo nome do artista \n4. Inserção pelo nome do artista\n-------------------------"); 
+        System.out.println("-------------------\nDigite qual metodo de ordenação deseja fazer: \n\n1. Seleção pelo nome da música\n2. Seleção pelo nome do artista \n3. Inserção pelo nome da música \n4. Inserção pelo nome do artista\n-------------------------\n" + //
+            "5. Bubblesort pelo nome da musica\n6. Bubblesort pelo nome do artista\n7. Shellsort pelo nome da musica\n8. Shellsort pelo nome do artista\n9. Quicksort pelo nome da musica\n10. Quicksort pelo nome do Artista\n-------------------------\n" + //
+            "11. Heapsort pelo nome da musica\n12. Heapsort pelo nome do artista\n13. Mergesort pelo nome da musica\n14. Heapsort pelo nome do Artista \n-------------------------"); 
         int op = sc.nextInt();
         
         switch(op){
           case 1:
-            System.out.println("Seleção pelo nome da música: ");
+            System.out.println("Seleção pelo nome da música: \n-------------------");
             od.SelecaoMusica();
             break;
           case 2:
-            System.out.println("Inserção pelo nome da música: ");
-            od.InsercaoMusica();
-            break;
-          case 3:
-            System.out.println("Seleção pelo nome do artista: ");
+            System.out.println("Seleção pelo nome do artista: \n-------------------");
             od.SelecaoArtista();
             break;
+          case 3:
+            System.out.println("Inserção pelo nome da música: \n-------------------");
+            od.InsercaoMusica();
+            break;
           case 4:
-            System.out.println("Inserção pelo nome do artista: ");
+            System.out.println("Inserção pelo nome do artista: \n-------------------");
             od.InsercaoArtista();
+            break;
+          case 5:
+            System.out.println("Bubblesort pelo nome da música: \n-------------------");
+            od.BubblesortMusica();
+            break;
+          case 6:
+            System.out.println("Bubblesort pelo nome do artista: \n-------------------");
+            od.BubblesortArtista();
+            break;
+          case 7:
+            System.out.println("Shellsort pelo nome da música: \n-------------------");
+            od.ShellsortMusica();
+            break;
+          case 8:
+            System.out.println("Shellsort pelo nome do artista: \n-------------------");
+            od.ShellsortArtista();
+            break;
+          case 9:
+            System.out.println("Quicksort pelo nome da música: \n-------------------");
+            ArrayList<Musica> lista = new ArrayList<>();
+            lista = lr.ListarMusicas();
+            int n = lista.size();
+            String nomeMusic[] = new String[n];
+            int left = 0;
+            int right = n-1;
+
+            for(int g=0; g<n; g++){
+                nomeMusic[g] = lista.get(g).getNome_musica();
+                nomeMusic[g].trim();
+            
+            }
+            od.QuicksortMusica(nomeMusic,left,right);
+            for(int g=0; g<n; g++){
+              System.out.println(nomeMusic[g]); 
+            }
+            break;
+
+          case 10:
+            System.out.println("Quicksort pelo nome do artista: \n-------------------");
+            ArrayList<Musica> lista2 = new ArrayList<>();
+            lista2 = lr.ListarMusicas();
+            int n2 = lista2.size();
+            String nomeArtista[] = new String[n2];
+            int left2 = 0;
+            int right2 = n2-1;
+
+            for(int g=0; g<n2; g++){
+                nomeArtista[g] = lista2.get(g).getNome_artista();
+                nomeArtista[g].trim();
+            
+            }
+            od.QuicksortArtista(nomeArtista,left2,right2);
+            for(int g=0; g<n2; g++){
+              System.out.println(nomeArtista[g]); 
+            }
+            break;
+          case 11:
+            System.out.println("Heapsort pelo nome da música: \n-------------------");
+            od.HeapsortMusica();
+            break;
+          case 12:
+            System.out.println("Heapsort pelo nome do artista: \n-------------------");
+            od.HeapsortArtista();
+            break;
+          case 13:
+            System.out.println("Mergesort pelo nome da música: \n-------------------");
+            od.MergesortMusica();
+            break;
+          case 14:
+            System.out.println("Mergesort pelo nome do artista: \n-------------------");
+            od.MergesortArtista();
             break;
           default:
             break;
--- src/LeituraCSV/Ordenacao.java
+++ src/LeituraCSV/Ordenacao.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Ordenacao extends LerArquivo{
     
+    
 
     public void SelecaoMusica(){
         // instanciando lista do CSV
@@ -129,4 +130,194 @@ public class Ordenacao extends LerArquivo{
         }
         
     }
+
+    public void BubblesortMusica(){
+        ArrayList<Musica> lista = new ArrayList<>();
+        lista = ListarMusicas();
+        int n = lista.size();
+        String nomeMusic[] = new String[n];
+        for(int g=0; g<n; g++){
+            nomeMusic[g] = lista.get(g).getNome_musica();
+            nomeMusic[g].trim();
+            
+        } 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nomeMusic[j].compareTo(nomeMusic[j+1]) > 0){
+                    String aux = nomeMusic[j];
+        		    nomeMusic[j]=nomeMusic[j+1];	
+        		    nomeMusic[j+1]= aux;
+                }
+            }
+        }
+        for(int g=0; g<n; g++){
+            System.out.println(nomeMusic[g]); 
+        }
+
+    }
+    public void BubblesortArtista(){
+        ArrayList<Musica> lista = new ArrayList<>();
+        lista = ListarMusicas();
+        int n = lista.size();
+        String nomeArtista[] = new String[n];
+        for(int g=0; g<n; g++){
+            nomeArtista[g] = lista.get(g).getNome_artista();
+            nomeArtista[g].trim();
+            
+        } 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nomeArtista[j].compareTo(nomeArtista[j+1]) > 0){
+                    String aux = nomeArtista[j];
+        		    nomeArtista[j]=nomeArtista[j+1];	
+        		    nomeArtista[j+1]= aux;
+                }
+            }
+        }
+        for(int g=0; g<n; g++){
+            System.out.println(nomeArtista[g]); 
+        }
+        
+    }
+    public void ShellsortMusica(){
+        ArrayList<Musica> lista = new ArrayList<>();
+        lista = ListarMusicas();
+        int n = lista.size();
+        String nomeMusic[] = new String[n];
+        int h = 1;
+
+        for(int g=0; g<n; g++){
+            nomeMusic[g] = lista.get(g).getNome_musica();
+            nomeMusic[g].trim();
+            
+        }
+    
+        while(h < n) {
+            h = h * 3 + 1;
+        }
+    
+        h = h / 3;
+        int j;
+        String c;
+        
+        while (h > 0) {
+            for (int i = h; i < n; i++) {
+                c = nomeMusic[i];
+                j = i;
+                while (j >= h && (nomeMusic[j-h].compareTo(c) > 0)) {
+                    nomeMusic[j] = nomeMusic[j - h];
+                    j = j - h;
+                }
+                nomeMusic[j] = c;
+            }
+            h = h / 2;
+        }
+        for(int g=0; g<n; g++){
+            System.out.println(nomeMusic[g]); 
+        }
+
+    }
+    public void ShellsortArtista(){
+        ArrayList<Musica> lista = new ArrayList<>();
+        lista = ListarMusicas();
+        int n = lista.size();
+        String nomeArtista[] = new String[n];
+        int h = 1;
+
+        for(int g=0; g<n; g++){
+            nomeArtista[g] = lista.get(g).getNome_artista();
+            nomeArtista[g].trim();
+            
+        }
+    
+        while(h < n) {
+            h = h * 3 + 1;
+        }
+    
+        h = h / 3;
+        int j;
+        String c;
+        
+        while (h > 0) {
+            for (int i = h; i < n; i++) {
+                c = nomeArtista[i];
+                j = i;
+                while (j >= h && (nomeArtista[j-h].compareTo(c) > 0)) {
+                    nomeArtista[j] = nomeArtista[j - h];
+                    j = j - h;
+                }
+                nomeArtista[j] = c;
+            }
+            h = h / 2;
+        }
+        for(int g=0; g<n; g++){
+            System.out.println(nomeArtista[g]); 
+        }
+        
+    }
+    public void QuicksortMusica(String nomeMusic[],int left, int right){
+
+        int i = left, j = right;
+        String tmp;
+        String pivot = nomeMusic[(left + right) / 2];
+
+        while (i <= j) {
+            while (nomeMusic[i].compareTo(pivot) < 0)
+                i++;
+            while (nomeMusic[j].compareTo(pivot) > 0)
+                j--;
+            if (i <= j) {
+                tmp = nomeMusic[i];
+                nomeMusic[i] = nomeMusic[j];
+                nomeMusic[j] = tmp;
+                i++;
+                j--;
+            }
+            };
+            /* recursion */
+            if (left < j)
+            QuicksortMusica(nomeMusic, left, j);
+            if (i < right)
+            QuicksortMusica(nomeMusic, i, right);
+            
+            
+    }
+    public void QuicksortArtista(String nomeArtista[],int left2,int right2){
+        int i = left2, j = right2;
+        String tmp;
+        String pivot = nomeArtista[(left2 + right2) / 2];
+
+        while (i <= j) {
+            while (nomeArtista[i].compareTo(pivot) < 0)
+                i++;
+            while (nomeArtista[j].compareTo(pivot) > 0)
+                j--;
+            if (i <= j) {
+                tmp = nomeArtista[i];
+                nomeArtista[i] = nomeArtista[j];
+                nomeArtista[j] = tmp;
+                i++;
+                j--;
+            }
+            };
+            /* recursion */
+            if (left2 < j)
+            QuicksortMusica(nomeArtista, left2, j);
+            if (i < right2)
+            QuicksortMusica(nomeArtista, i, right2);
+        
+    }
+    public void HeapsortMusica(){
+
+    }
+    public void HeapsortArtista(){
+        
+    }
+    public void MergesortMusica(){
+
+    }
+    public void MergesortArtista(){
+        
+    }
+
 }
